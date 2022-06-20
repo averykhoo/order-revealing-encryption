35c0ce1fbd57925df5156851fcef9e1e33b586ab
Avery Khoo <averykhoo@gmail.com>
2022-06-20 10:41:44 +0800

    test small domain ore

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # order-revealing-encryption
+
+from the paper 
+[Order-Revealing Encryption: New Constructions, Applications, and Lower Bounds](https://www.cs.utexas.edu/~dwu4/papers/BlockORE.pdf)
---
+++ small_domains.py
@@ -0,0 +1,146 @@
+"""
+based on Section 3: ORE for Small Domains
+"""
+import random
+
+N = 4096  # space of all possible messages
+lmbda = 64  # security parameter
+compare_size = 3  # possible outcomes for cmp
+
+
+def cmp(left, right):
+    """
+    make sure to also modify `compare_size`
+    more correct would be to use an enum.IntEnum
+
+    :param left:
+    :param right:
+    :return:
+    """
+    if left == right:
+        return 0 % compare_size
+    if left > right:
+        return 1 % compare_size
+    if left < right:
+        return -1 % compare_size
+
+
+def F(secret_key, data):
+    """
+    Let F : {0, 1} ** λ × {0, 1} ** λ → {0, 1} ** λ be a secure PRF
+
+    The correct implementation for this would probably be a HMAC of some kind
+
+    :param secret_key:
+    :param data:
+    :return:
+    """
+    return hash(('salt', secret_key, data)) & ((1 << lmbda) - 1)
+
+
+def H(data1, data2):
+    """
+    H : {0, 1} ** λ × {0, 1} ** λ → Z₃ be a hash function (modeled as a random oracle in the security proof)
+
+    The correct implementation of this would probably be a cryptographic hash like SHA
+
+    :param data:
+    :return:
+    """
+    return hash(('salt', data1, data2)) % compare_size
+
+
+def permute(permutation, data):
+    return permutation[data]
+
+
+def ore_setup():
+    """
+    On input a security parameter λ, the setup algorithm outputs a secret key sk.
+    The setup algorithm samples a PRF key k ← {0, 1} ** λ for F,
+        and a uniformly random permutation π : [N] → [N].
+    The secret key sk is the pair (k, π).
+
+    :return:
+    """
+    k = random.randint(0, (1 << lmbda) - 1)
+    pi = list(range(N))
+    random.shuffle(pi)
+    return k, pi
+
+
+def ore_encrypt_left(secret_key, message):
+    """
+    On input a secret key sk and a message m ∈ D, the encryption algorithm outputs a ciphertext ct.
+    Write sk as (k, π).
+    The left encryption algorithm computes and returns the tuple ct(L) = (F(k, π(x)), π(x)).
+
+    :param secret_key:
+    :param message:
+    :return:
+    """
+    k, pi = secret_key
+    permuted_message = permute(pi, message)
+    return F(k, permuted_message), permuted_message
+
+
+def ore_encrypt_right(secret_key, message):
+    """
+    On input a secret key sk and a message m ∈ D, the encryption algorithm outputs a ciphertext ct.
+    Write sk as (k, π).
+    First, the right encryption algorithm samples a random nonce r ← {0, 1} ** λ.
+    Then, for each i ∈ [N], it computes the value v(i) = cmp(π⁻¹(i), y) + H(F(k, i), r) (mod 3).
+    Finally, it outputs the ciphertext ct(R) = (r, v(1), v(2), ... , v(N)).
+
+    :param secret_key:
+    :param message:
+    :return:
+    """
+
+    # invert pi
+    k, pi = secret_key
+    pi_inverse = [-1] * len(pi)
+    for i, x in enumerate(pi):
+        pi_inverse[x] = i
+
+    # random nonce
+    nonce = random.randint(0, (1 << lmbda) - 1)
+
+    # compute each v(i)
+    # note: instead of computing for each ciphertext, we could instead compute for each plaintext
+    # but since this is how the paper is written, we'll do it this way
+    out = [nonce]
+    for ciphertext in range(N):
+        plaintext = permute(pi_inverse, ciphertext)
+        v_i = (cmp(plaintext, message) + H(F(k, ciphertext), nonce)) % compare_size
+        out.append(v_i)
+    return tuple(out)
+
+
+def ore_compare(ciphertext_left, ciphertext_right):
+    """
+    On input two ciphertexts ct1, ct2, the compare algorithm outputs a bit b ∈ {0, 1}.
+    The compare algorithm first parses ct(L) = (k', h) and ct(R) = (r, v(1), v(2), ... , v(N)),
+        and then outputs the result v(h) − H(k', r) (mod 3).
+
+    :param ciphertext_left:
+    :param ciphertext_right:
+    :return:
+    """
+    k_prime, h = ciphertext_left
+    nonce = ciphertext_right[0]
+    v_i = ciphertext_right[h + 1]
+    return (v_i - H(k_prime, nonce)) % compare_size
+
+
+if __name__ == '__main__':
+    sk = ore_setup()
+    print(f'{sk=}')
+    for j in range(N):
+        print(j)
+        ct_r = ore_encrypt_right(sk, j)
+        for i in range(N):
+            ct_l = ore_encrypt_left(sk, i)
+            cmp_result = ore_compare(ct_l, ct_r)
+            assert cmp(i, j) == cmp_result
+            # print(i, j, ct_l, ct_r)
